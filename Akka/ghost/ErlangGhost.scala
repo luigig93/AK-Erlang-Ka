899c3ef746432b24f74813ad5285e539365409bc
Luigi Grossi <luigig93@gmail.com>
2020-10-22 20:43:55 +0200

    aggiunti parametri di tipo per risolvere problema tipaggio ActorRef

--- Akka/ghost/ErlangGhost.scala
+++ Akka/ghost/ErlangGhost.scala
@@ -1,50 +1,34 @@
-package ghost
+package actors
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
 import com.ericsson.otp.erlang.{OtpErlangAtom, OtpErlangPid, OtpMbox, OtpNode}
 
-import scala.reflect.runtime.{universe => u}  // reflection
-
 object ErlangGhost {
 
-  def apply(node: OtpNode, erlangPid: OtpErlangPid, expMsgType: u.Type): Behavior[Any] =
+  def apply[M](node: OtpNode, erlangPid: OtpErlangPid): Behavior[M] =
     Behaviors.setup { _ =>
       // creaiamo la mailbox che ci servirà per la send
-      val mbox = node.createMbox() // la usiamo solo per fare send, non serve registrarle localmente
-      ghostLoop(mbox, erlangPid, expMsgType)
+      val mbox = node.createMbox() // la usiamo solo per fare send, non serve registrarla localmente
+      ghostLoop[M](mbox, erlangPid)
     }
 
-
-  def ghostLoop(mbox: OtpMbox, erlangPid: OtpErlangPid, expMsgType: u.Type): Behavior[Any] =
+  def ghostLoop[M](mbox: OtpMbox, erlangPid: OtpErlangPid): Behavior[M] =
   // un ghost actor ricopre il ruolo del vecchio Send Broker
   // deve aspettare messaggi provenienti da un altro attore Akka
   // facendo finta di essere un attore Akka, ed inoltrare i messaggi (dopo encoding) ad Erlang
 
   Behaviors.receiveMessage {
-      msg: Any =>
-        // reflection
-        val mirror = u.runtimeMirror(getClass.getClassLoader)
-        val className = msg.getClass.getName
-        val classSymbol = mirror.staticClass(className)
-        val clsType = classSymbol.toType
-        // qui simula il controllo di tipo dei messaggi Akka
-        // se si vuole bloccare l'esecuzione, allora usare un assert
-        if(clsType <:< expMsgType) {
-          // fai encoding di msg
-          println("new msg...")
-          // val erlangMsg = encode(msg)
-          println("encode...")
-          // invialo a Erlang
-          mbox.send(erlangPid, new OtpErlangAtom("msg"))
-        } else {
-          // segnala l'errore
-          println("msg type error...")
-          // e ora, che si fa?
-          // - accettare il fallimento e andare avanti
-          // - propagare l'eccezione
-          // - terminare l'esecuzione
-        }
+    case msg: M =>
+      // fai encoding di msg
+      println("new msg...")
+      // val erlangMsg = encode(msg)
+      println("encode...")
+      // invialo a Erlang
+      mbox.send(erlangPid, new OtpErlangAtom("msg"))
+      ghostLoop[M](mbox, erlangPid)
 
-        ghostLoop(mbox, erlangPid, expMsgType)
+    case _ =>
+      println("errore di tipo: l'attore non è in grado di gestire questo messaggio")
+      ghostLoop[M](mbox, erlangPid)
     }
 }
